5397cc8527f3a50409d33341423421e084f1241b
Tucker2015 <kevtucker@gmail.com>
2023-08-05 11:34:38 +0100

    - Remove deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-GIT_USER=Tucker2015 GIT_PASS=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 DEPLOYMENT_BRANCH=master yarn deploy  
+GIT_USER=Tucker2015 GIT_PASS=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 DEPLOYMENT_BRANCH=master yarn deploy  
