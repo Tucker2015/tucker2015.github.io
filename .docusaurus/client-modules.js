32c7251564888bbb582ef89b4239f0173402f0da
Tucker2015 <kevtucker@gmail.com>
2023-08-05 11:51:07 +0100

    Deploy website - based on eac5736cc6000ad7b35b095bf0e75223ebadc27c

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/kevin/Documents/Github/tucker2015.github.io/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/kevin/Documents/Github/tucker2015.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/kevin/Documents/Github/tucker2015.github.io/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/Users/kevin/Documents/Github/tucker2015.github.io/src/css/custom.css"),
-];
